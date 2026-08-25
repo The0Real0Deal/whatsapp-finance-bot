24f9f1650e50c839ae2a2a704ed5932441d74764
H15 <167916692+The0Real0Deal@users.noreply.github.com>
2026-08-25 20:58:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Bot is active and running!'));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
+
+
 // אתחול Gemini API
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: 'gemini-1.5-flash',
+  model: 'gemini-2.5-flash',
   generationConfig: { responseMimeType: 'application/json' }
 });
 
